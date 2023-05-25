b90ae0e0552b515104b0e20e16f920380bdcfb5c
Muhammad Adil Raja Saputra <adilraja19@students.undip.ac.id>
2023-05-25 16:10:01 +0700

    Add config for Bundle Analyzer

--- webpack.common.js
+++ webpack.common.js
@@ -67,6 +67,9 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
   ],
 };
